5ca847a31496477448c11987f8219b91363491ce
Dmitry Holmachev <dimonsholm@gmail.com>
2025-04-25 13:50:12 +0300

    dashboardPage css added

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -3,18 +3,16 @@ import { Link, useNavigate } from "react-router-dom";
 
 const DashboardPage = () => {
   return (
-    <div className="dashboard">
-      <h1>Добро пожаловать!</h1>
-      <div className="dashboard-container">
-        <p>Выберите, что хотите изучать:</p>
-        <div className="links">
-          <Link to="/theory" className="btn">
-            <button>Перейти к теории</button>
-          </Link>
-          <Link to="/tasks" className="btn">
-            <button>Перейти к заданиям</button>
-          </Link>
-        </div>
+    <div className="dashboard-container">
+      <h1 className="dashboard-title">Добро пожаловать!</h1>
+      <p className="dashboard-description">Выберите, что хотите изучать:</p>
+      <div className="dashboard-buttons">
+        <Link to="/theory" className="dashboard-button">
+          <button>Перейти к теории</button>
+        </Link>
+        <Link to="/tasks" className="dashboard-button">
+          <button>Перейти к заданиям</button>
+        </Link>
       </div>
     </div>
   );
